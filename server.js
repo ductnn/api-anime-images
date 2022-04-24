0756a40509b7383a98e9af2e56bbb27038bd756b
ductnn <trannhuduc531998@gmail.com>
2022-04-25 03:05:29 +0700

    feat: update anime route

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 const express = require("express");
-
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = new express();
+
+//Routes
+const animeRoute = require('./routes/api');
+
+//middlewares
+app.use(cors());
+app.use(morgan('tiny'))
+app.use(express.json());
+
+app.use('/api/v1', animeRoute);
 
 app.get("/", (req, res) => {
     res.send('<h1> API Worked !!!!! </h1>');
